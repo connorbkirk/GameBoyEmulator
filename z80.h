e7aef41c8fa3a4c35a887145ae4ea06cb0229789
CONNOR <ckirk96@gmail.com>
2017-05-15 07:58:27 -0400

    updated var types

--- z80.h
+++ z80.h
@@ -7,18 +7,18 @@
 #define REG_C 2
 #define REG_D 3
 #define REG_E 4
-#define REG_F 5
-#define REG_G 6
-#define REG_H 7
+#define REG_H 5
+#define REG_L 6
+#define REG_F 7
 #define FLAG_ZERO 7
 #define FLAG_SUB 5
 #define FLAG_HALF_CARRY 5
 #define FLAG_CARRY 4
 
 typedef struct{
-        int16_t registers[8];
-        unsigned char display[23040];
-        int16_t memory[65535];
+        uchar registers[8];
+        uchar display[23040];
+        uchar memory[65535];
         int16_t sp;
         int16_t pc;
         bool need_redraw;
