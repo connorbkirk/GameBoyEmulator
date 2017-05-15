bae031b87d08a3b7c8de9edca0b044cbc8e9ef8c
CONNOR <ckirk96@gmail.com>
2017-05-15 08:52:18 -0400

    added support for additional opcodes

--- z80.c
+++ z80.c
@@ -1,6 +1,8 @@
 /* represents the z80 microcontroller that the game boy utilizes */
 
 #include "z80.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 void reset_z80( z80_t * z80 ){
 	memset( z80->display, 0, 23040 );
@@ -10,21 +12,45 @@ void reset_z80( z80_t * z80 ){
 	z80->sp = 0xFFFE;
 }
 
-void push_z80( z80_t * z80, uchar n){
+void push_z80( z80_t * z80, char n){
 	z80->memory[z80->sp] = n;
 	z80->sp--;
 }
 
-uchar pop_z80( z80_t * z80 ){
-	return z80->memory[z80->sp++];
+unsigned char pop_z80( z80_t * z80 ){
+	z80->sp++;
+	return z80->memory[z80->sp];
 }
 
 int16_t combine_reg(z80_t * z80, int left, int right){
 	return z80->registers[left] << 8 | z80->registers[right];
 }
 
-void jump_z80(z80_t * z80, int16_t nn){
+bool check_cc(z80_t * z80, cc cond){
+	switch(cond){
+		case NZ:
+			return ((unsigned char)z80->registers[REG_F] >> FLAG_ZERO) == 0;
+		case Z:
+			return ((unsigned char)z80->registers[REG_F] >> FLAG_ZERO) == 1;
+		case NC:
+			return ((unsigned char)z80->registers[REG_F] >> FLAG_CARRY) == 0;
+		case C:
+			return ((unsigned char)z80->registers[REG_F] >> FLAG_CARRY) == 1;
+		default:
+			printf("Condition is not valid\n");
+			exit(1);
+	}
+
+}
+
+void jump_nn_z80(z80_t * z80, int16_t nn){
 	z80->pc = nn;
+}
+
+void jump_cc_z80(z80_t * z80, cc cond, int16_t nn){
+	if( check_cc(z80, cond) ){
+		jump_nn_z80(z80, nn);
+	}
 }
 
 void call_z80(z80_t * z80, int16_t nn){
@@ -32,12 +58,13 @@ void call_z80(z80_t * z80, int16_t nn){
 	addr = z80->pc + 1;
 	push_z80( z80, addr >> 8 );
 	push_z80( z80, addr );
-	jump_z80( z80, nn );
+	jump_nn_z80( z80, nn );
 }
 
 int run_z80( z80_t * z80 ){
 	unsigned char opcode;
 	int16_t nn;
+	char n;
 	//todo - fetch opcode
 	opcode = 0x0;
 
@@ -97,15 +124,54 @@ int run_z80( z80_t * z80 ){
 
 		//CALL
 		case 0xCD:{//CALL nn
-			nn = z80->memory[z80->pc+1] + (z80->memory[z80->pc+2] << 8);
+			nn = z80->memory[z80->pc+1] | (z80->memory[z80->pc+2] << 8);
 			call_z80(z80, nn);
 			return 12;
+		}
+
+		//JMP
+		case 0xC3:{//JMP nn
+			nn = z80->memory[z80->pc+1] | (z80->memory[z80->pc+2] << 8);
+			jump_nn_z80(z80, nn);
+			return 12;
+			
+		}
+		case 0xC2:{//JMP NZ, nn
+			nn = z80->memory[z80->pc+1] | (z80->memory[z80->pc+2] << 8);
+			jump_cc_z80(z80, NZ, nn);
+			return 12;
+		}
+		case 0xCA:{//JMP Z, nn
+			nn = z80->memory[z80->pc+1] | (z80->memory[z80->pc+2] << 8);
+			jump_cc_z80(z80, Z, nn);
+			return 12;
+		}
+		case 0xD2:{//JMP NC, nn
+			nn = z80->memory[z80->pc+1] | (z80->memory[z80->pc+2] << 8);
+			jump_cc_z80(z80, NC, nn);
+			return 12;
+		}
+		case 0xDA:{//JMP C, nn
+			nn = z80->memory[z80->pc+1] | (z80->memory[z80->pc+2] << 8);
+			jump_cc_z80(z80, C, nn);
+			return 12;
+		}
+		case 0xE9:{//JMP HL
+			nn = combine_reg(z80, REG_H, REG_L);
+			jump_nn_z80(z80, nn);
+			return 4;
+		}
+		case 0x18:{//JR n
+			n = z80->memory[z80->pc + 1];	
+			nn = z80->pc + n;
+			jump_nn_z80(z80, nn);
+			return 8;
 		}
 
 		//RETURN
 		case 0xC9:{//RET
 			nn = pop_z80(z80) | (pop_z80(z80) << 8);
-			jump_z80(z80, nn);
+			jump_nn_z80(z80, nn);
 			return 8;
 		}
 		
--- z80.h
+++ z80.h
@@ -11,14 +11,14 @@
 #define REG_L 6
 #define REG_F 7
 #define FLAG_ZERO 7
-#define FLAG_SUB 5
+#define FLAG_SUB 6
 #define FLAG_HALF_CARRY 5
 #define FLAG_CARRY 4
 
 typedef struct{
-        uchar registers[8];
-        uchar display[23040];
-        uchar memory[65535];
+        char registers[8];
+        unsigned char display[23040];
+        char memory[65535];
         int16_t sp;
         int16_t pc;
         bool need_redraw;
@@ -30,3 +30,6 @@ void load_game( z80_t * z80, char * name );
 void repaint_z80(z80_t * z80);
 void reset_draw_flag(z80_t * z80);
 
+typedef enum{
+	NZ, Z, NC, C
+} cc;
